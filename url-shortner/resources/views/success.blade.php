ebe822a6d230d9bc44ea6bff3c268172b25f4da2
subhash patidar <subhashpatidar2002@gmail.com>
2025-03-28 17:11:10 +0530

    Update success.blade.php

--- url-shortner/resources/views/success.blade.php
+++ url-shortner/resources/views/success.blade.php
@@ -6,20 +6,25 @@
     <h2>Short Link Generated Successfully</h2>
     <div class="mb-3">
         <input type="text" class="form-control" value="{{ session('short_url') }}" id="shortUrl">
-        <button class="btn btn-secondary mt-2" onclick="copyToClipboard">Copy Url</button>
+        <button class="btn btn-secondary mt-2" onclick="copyToClipboard()">Copy Url</button>
     </div>
 </div>
 
 <a href="/" class="btn btn-success">Create Another</a>
 <a href="/links" class="btn btn-info">View All Links</a>
 
-
 <script>
-    function copyToClipboard(){
+    function copyToClipboard() {
         var copyText = document.getElementById("shortUrl");
         copyText.select();
-        document.execCommand("copy");
-        alert("copied to clipboard")
+        copyText.setSelectionRange(0, 99999); // Mobile ke liye
+
+        navigator.clipboard.writeText(copyText.value).then(() => {
+            alert("Copied to clipboard!");
+        }).catch(err => {
+            console.error("Failed to copy: ", err);
+        });
     }
 </script>
+
 @endsection
